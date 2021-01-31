8aef3920a17b2314994ff31387d9c593ef3c3952
titus nangi <titusnangi@gmail.com>
2021-01-31 02:58:50 +0200

    Initial commit

---
+++ src/com/titoacademy/Main.java
@@ -0,0 +1,12 @@
+package com.titoacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String myString = " MY NAME IS TITUS NANGI";
+        myString = myString.replaceAll("\\s","");
+        System.out.println(myString);
+
+    }
+}
